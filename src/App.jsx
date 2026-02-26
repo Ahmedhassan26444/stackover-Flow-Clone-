00ac2663a5acd2307cbb40f9bb2b075f778288ff
Ahmad <ahmedhassan.26444@gmail.com>
2026-02-26 22:42:59 +0500

    add the main-context and updated app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,21 @@ import Navbar from "./Components/Navbar"
 import NewestQuestion from "./Components/NewestQuestion"
 import Rightwidgets from "./Components/Rightwidgets"
 import Footer from "./Components/Footer"
+import MainContent from "./Components/MainContent"
 
 function App() {
   return (
     <>
       <Header />
-
       <div className="flex">
-
         <Navbar />
-
         <div className="flex flex-1 px-8">
-
           <div className="flex-1">
             <NewestQuestion />
+            <MainContent /> 
           </div>
-
-          <Rightwidgets />
-
+          <Rightwidgets />  
         </div>
-
       </div>
       <Footer />
     </>
