c0d8bb46f748553ee82f0a4f8757714f08c275a2
Ahmad <ahmedhassan.26444@gmail.com>
2026-02-28 01:35:00 +0500

    minor changes in overall  ui  like spacing adding button in header etc

--- src/Components/NewestQuestion.jsx
+++ src/Components/NewestQuestion.jsx
@@ -8,38 +8,41 @@ const NewestQuestion = () => {
         <h1 className='text-2xl font-semibold text-gray-800'>
           Newest Questions
         </h1>
-        <button className='bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-[#0074cc]'>
+        <button className='bg-[#1976D2] text-white rounded-md 
+         px-2 py-2 mr-3 text-sm hover:bg-[#1565C0]'>
           Ask Question
         </button>
       </div>
 
       {/* Row 2: Count + Tabs + Filter */}
-      <div className="flex items-center justify-between pb-4">
+      <div className="flex items-center justify-between  pb-4">
         <p className="text-gray-700 text-lg">
           24,172,760 questions
         </p>
-        <div className="flex items-center gap-2 ">
-          <div className="flex border rounded-md overflow-hidden text-sm">
+        <div className="flex items-center gap-2 mr-3  ">
+          <div className="flex border border-gray-400 rounded-md overflow-hidden text-[12px]">
             <button className="px-3 py-1.5 bg-gray-200 font-medium">
               Newest
             </button>
             <button className="px-3 py-1.5">
               Active
             </button>
-            <button className="px-3 py-1.5 flex flex-row items-center leading-tight">
+            <button className="px-3 py-1.5 flex flex-row
+             items-center leading-tight">
               <span className='mr-1'>Bountied</span>
-              <span className="bg-[#155CA2] text-white text-[11px] px-1.5 py-0.5
+              <span className="bg-[#155CA2] text-white text-[11px]
+               px-1.5 py-0.5
                rounded">11</span>
             </button>
             <button className="px-3 py-1.5">
               Unanswered
             </button>
-            <button className="px-3 py-1.5">
+            <button className="px-3 py-1.5 pr-5">
               More ▼
             </button>
           </div>
-          <button className="border border-blue-500 text-blue-500 px-2 py-2
-           rounded-md hover:bg-blue-50 flex items-center  gap-1 text-sm">
+          <button className="border  border-blue-500 text-blue-500 px-2 
+          py-2 ml-2 rounded-md hover:bg-blue-50 flex items-center  gap-1 text-sm">
             ☰ Filter
           </button>
         </div>
